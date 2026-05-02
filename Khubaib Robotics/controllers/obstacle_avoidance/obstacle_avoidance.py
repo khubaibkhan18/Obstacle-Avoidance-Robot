93b355683f3460a2f3bb1c24a284cc529558e498
khubaibkhan18 <149138545+khubaibkhan18@users.noreply.github.com>
2026-05-02 14:12:58 +0500

    Addition of Camera, frame, and wheel sensors

--- Khubaib Robotics/controllers/obstacle_avoidance/obstacle_avoidance.py
+++ Khubaib Robotics/controllers/obstacle_avoidance/obstacle_avoidance.py
@@ -1,7 +1,15 @@
 from controller import Robot
+import numpy as np
 
 SPEED = 7.0
 TIME_STEP = 64
+
+def get_frame(camera, w, h):
+    raw = camera.getImage()
+    if raw is None:
+        return None
+    img = np.frombuffer(raw, dtype=np.uint8).reshape((h, w, 4))
+    return img[:, :, :3]
 
 def main():
     robot = Robot()
@@ -13,9 +21,20 @@ def main():
     left_motor.setVelocity(0)
     right_motor.setVelocity(0)
 
-    print("[INFO] Robot starting...")
+    left_sensor  = robot.getDevice("left wheel sensor")
+    right_sensor = robot.getDevice("right wheel sensor")
+    left_sensor.enable(TIME_STEP)
+    right_sensor.enable(TIME_STEP)
+
+    camera = robot.getDevice("camera")
+    camera.enable(TIME_STEP)
+    cam_w = camera.getWidth()
+    cam_h = camera.getHeight()
+
+    print("[INFO] Camera enabled, starting...")
 
     while robot.step(TIME_STEP) != -1:
+        frame = get_frame(camera, cam_w, cam_h)
         left_motor.setVelocity(SPEED)
         right_motor.setVelocity(SPEED)
 
